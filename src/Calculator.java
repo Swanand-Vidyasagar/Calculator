5e6a13ada88d20f75a2bbc91fec89e77c882115b
Swanand <swanandvsagar@gmail.com>
2025-02-18 23:49:11 +0530

    Started the project

---
+++ src/Calculator.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+
+public class Calculator implements ActionListener {
+
+    JFrame frame;
+    JTextField textfield;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton;
+    JPanel panel;
+
+    Font myFont = new Font("Ink Free", Font.BOLD,30);
+    
+    Calculator(){
+
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
